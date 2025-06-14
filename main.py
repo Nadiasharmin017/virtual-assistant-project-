df9b1357c15cc1461eca51c7bd84770ec2b8a332
Nadiasharmin017 <nadiasharmin017@gmail.com>
2025-06-14 16:50:49 +0600

    create info section from wikipedia

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import speech_recognition as sr
 import pyttsx3
 import datetime
 import pywhatkit
+import wikipedia
 
 listener = sr.Recognizer( )
 alexa = pyttsx3.init()
@@ -10,12 +11,14 @@ def talk(text):
     alexa.say(text)
     alexa.runAndWait()    
 
-def take_command( ): 
+def take_command(): 
+    command = ""    
     try:
         with sr.Microphone() as source:
             print("listening...") 
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
+            command = command.lower()
             if "alexa" in command:
                 command = command.replace("alexa", "")  
     except:
@@ -34,7 +37,10 @@ def run_alexa():
         talk("playing" + song)
         pywhatkit.playonyt(song) 
           
-        
-        
+    elif "tell me about" in command: 
+            look_for = command.replace("tell me about", "")  
+            info = wikipedia.summary(look_for, 1)
+            print(info) 
+            talk(info)
 
 run_alexa() 
