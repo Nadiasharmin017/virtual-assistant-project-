7cc118341b3499dc0851abd6ce6abecde4a23699
Nadiasharmin017 <nadiasharmin017@gmail.com>
2025-06-14 14:46:55 +0600

    creating current time to talk alexa

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import speech_recognition as sr
-listener = sr.Recognizer( ) 
+import pyttsx3
+import datetime
 
-try:
-    with sr.Microphone() as source:
-        print("listening...") 
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        print(command)     
-except:
-    pass
+listener = sr.Recognizer( )
+alexa = pyttsx3.init()
+
+def talk(text): 
+    alexa.say(text)
+    alexa.runAndWait()    
+
+def take_command( ): 
+    try:
+        with sr.Microphone() as source:
+            print("listening...") 
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            # if "alexa" in command:
+            print(command)     
+    except:
+        pass
+    return command
+# if ask time anyone alexa tall time now 
+def run_alexa():
+    command = take_command()
+    if 'time' in command:
+        time = datetime.datetime.now().strftime("%I:%M %p") 
+        print(time)  
+        talk("current time is" + time)   
+        
+
+run_alexa() 
